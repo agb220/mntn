322278ad920f3f5fa75827672e3ae0b0bfd99534
agb220 <annabalabonchik@gmail.com>
2025-06-25 19:07:25 +0300

    create header, add  SVG-спрайт

--- js/main.js
+++ js/main.js
@@ -19,3 +19,26 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("footer").innerHTML = data;
     });
 });
+
+// add icons SVG-спрайт
+
+fetch("icons/icons-sprite.svg")
+  .then((res) => res.text())
+  .then((data) => {
+    const div = document.createElement("div");
+    div.style.display = "none";
+    div.innerHTML = data;
+    document.body.prepend(div);
+  });
+
+// open menu-burger
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest(".icon-menu")) {
+    document.body.classList.toggle("menu-open");
+  }
+}
